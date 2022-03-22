629d007faeb4daba875ebce69c5c102c22542f55
Aniket Mandle <mandle.aniket2@gmail.com>
2022-03-22 15:11:05 +0530

    #100002 - Render customers Review Dynamically

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-
+customerList:any =[];
   constructor() { }
 
   ngOnInit(): void {
+
+    const customerObj1: any = {
+      "custReview": "This book was pretty helpful in giving me ideas to write in the SAT.However, the length of the examples are not consistent.Some are detailed while others are rather short",
+      "custName": "Anup Mangalkar",
+      "custImg": "./../assets/images/Photo.jpeg",
+    };
+    const customerObj2: any = {
+      "custReview": "This book was pretty helpful in giving me ideas to write in the SAT.However, the length of the examples are not consistent.Some are detailed while others are rather short",
+      "custName": "Ankush Manjare",
+      "custImg": "./../assets/images/Photo.jpeg",
+
+    };
+    const customerObj3: any = {
+      "custReview": "This book was pretty helpful in giving me ideas to write in the SAT.However, the length of the examples are not consistent.Some are detailed while others are rather short",
+      "custName": "Ashish Manjare",
+      "custImg": "./../assets/images/Photo.jpeg",
+    };
+
+this.customerList.push(customerObj1);
+this.customerList.push(customerObj2);
+this.customerList.push(customerObj3);
+
   }
 
 }
